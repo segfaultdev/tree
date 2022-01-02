2519ce3fc80f1a9f1bd67df8b664641c1e3f90d4
superleaf1995 <jesusantonio30122016@gmail.com>
2022-01-01 19:14:37 -0600

    windows build script
    Signed-off-by: superleaf1995 <jesusantonio30122016@gmail.com>

---
+++ build_win.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+# lib/windows should have the win32 libraries inside
+# use msys2 for this
+
+gcc -O3 -static -s $(find . -name "*.c") -Llib -Iinclude -lraylib -lz -lm -lwsock32 -lws2_32 -lglfw3 -lwinpthread -lglew32 -lopengl32 -lglu32 -luser32 -lgdi32 -lole32 -loleaut32 -limm32 -lwinmm -lversion -o tree
